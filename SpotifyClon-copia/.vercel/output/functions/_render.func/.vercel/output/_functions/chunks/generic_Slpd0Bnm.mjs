d88baeb564ae6d779f79876f3986683d303265b0
Adrián <aadri1620@gmail.com>
2024-05-18 12:56:03 +0200

    asd

---
+++ SpotifyClon-copia/.vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Slpd0Bnm.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_GxTh_bW1.mjs');
+
+export { page };
---
+++ SpotifyClon-copia/.vercel/output/static/_astro/Player.CiYnkIo_.js
@@ -0,0 +1,1 @@
+import{b as e}from"./Player.KmzFgRm-.js";import"./index.DGSwqXIp.js";export{e as Player};
