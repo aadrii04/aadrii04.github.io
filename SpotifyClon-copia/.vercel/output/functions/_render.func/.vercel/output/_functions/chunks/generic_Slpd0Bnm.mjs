d6d1b51b64fd147c4fbcb0d828b6ff6f09f91c16
Adrián <aadri1620@gmail.com>
2024-06-02 16:33:13 +0200

    asdasd

--- SpotifyClon-copia/.vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_Slpd0Bnm.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_GxTh_bW1.mjs');
-
-export { page };
--- SpotifyClon/astro.config.mjs
+++ SpotifyClon/astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
